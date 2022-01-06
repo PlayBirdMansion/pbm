9ba307eb642251b9bef2b368c7caecac916bdc5a
PlayBirdMansion <89541118+PlayBirdMansion@users.noreply.github.com>
2022-01-06 15:45:11 -0500

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,7 +12,7 @@ export const Footer: React.FC<FooterProps> = ({ }) => {
   return (
     <footer className='text-white flex flex-col items-center justify-center text-f1 pb-3 font-light mt-24'>
       <div className='flex flex-row'>
-        <a href={'https://twitter.com/PlayBirdMansion'} target='_blank' rel="noreferrer">
+        <a href={'https://twitter.com/alienfrens'} target='_blank' rel="noreferrer">
           <TwitterFill size='30' />
         </a>
         <div className='py-4 px-2' />
